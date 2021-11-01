c57f35653f7369c887794e0dd0bf53f351627b30
Renato Beltran <rbeltranwodeva@gmail.com>
2021-10-31 21:31:23 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import Todo from "./components/Todo";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "learn react",
+      isCompleted: false,
+    },
+    {
+      text: "meet friend for lunch",
+      isCompleted: false,
+    },
+    {
+      text: "build todo app",
+      isCompleted: false,
+    },
+    {
+      text: "Take pictures",
+      isCompleted: false,
+    },
+    {
+      text: "Do exercies",
+      isCompleted: false,
+    },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [{ text: text, isCompleted: false }, ...todos];
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    let temp = [...todos];
+    temp.splice(index, 1);
+    setTodos(temp);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col xs={12} className="App">
+          <Card className="my-card">
+            <Card.Title className="my-app-title">
+              <h1>Todo List App</h1>
+            </Card.Title>
+            <Card.Body>
+              <div className="my-container">
+                <TodoForm addTodo={addTodo} />
+                {todos.map((todo, i) => (
+                  <Todo index={i} key={i} todo={todo} remove={removeTodo} />
+                ))}
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
